f049485b9066625f66159e2e6f889b0ac9ea71fc
Joshua Hill <joshuahillx1@gmail.com>
2022-09-16 11:36:00 -0500

    new entities

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ApplicationRecord
-	validates :title, :author, :price, :published_date, presence: true
+	has_many :users, through: :user_books
+	has_many :user_books
+
+	validates :title, presence: true
+	validates :author, presence: true
+	validates :price, presence: true
+	validates :published_date, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  root 'books#index'
-
+  resources :user_books
+  resources :users
   resources :books do
-    member do
-      get :delete
-    end
+  	member do
+  		get :delete
+  	end
   end
+  
+  root 'user_books#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
